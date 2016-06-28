aa20b662b92eafed09cd4d62e94704a50c5ad669
eduardosanzb <eduardosanzb@gmail.com>
2016-06-28 10:43:52 -0500

    some fixes

--- itam-angular/src/app/controllers/TableController.js
+++ itam-angular/src/app/controllers/TableController.js
@@ -3,27 +3,23 @@
   angular
     .module('app')
     .controller('TableController', [
-      'instanceService',
+      'instanceService', '$mdDialog',
       TableController
     ]);
 
-  function TableController(instanceService) {
-    /*  Template:   app/views/table.html
-     *  $state:     home.grading
-     *  - Variables
-     *  ..- limitOptions: Options for the pagination of the table
-     *  ..- filter: Empty object for the query filter of the table
-     *  ..- query: Options for the query/order/pagination of the table
-     *  ..- options: Options for the table structure
-     *  - Services
-     *  ..- studentService: Retrieving dummy data for the table
-     *  - Functions
-     *  ..- editGrade() :  Helper function to open the dialog in the table, will ng-model the grade
-     *      to the table object.  
-     *  ..- saveDraft() : button fnc to save the draft and go back to the table.
-     *  ..- printPdf() : button fnc to print a preview of the transaction pdf for the teacher.
-     *  ..- sendTransaction() : button fnc to POST the table-content object and go back to the list.
-     */
+  function TableController(instanceService, $mdDialog) {
+        /*  Template:   app/views/table.html
+         *  $state:     home.grading
+         *  - Variables
+         *  ..- limitOptions: Options for the pagination of the table
+         *  ..- filter: Empty object for the query filter of the table
+         *  ..- query: Options for the query/order/pagination of the table
+         *  ..- options: Options for the table structure
+         *  - Services
+         *  ..- studentService: Retrieving dummy data for the table
+         *  - Functions
+         *  ..- startTransaction() : Function to start the transactions selected
+         */
         var vm = this;
         /*INITIALIZING VARIABLES*/
           vm.selected = [];
@@ -45,11 +41,7 @@
                 pageSelect: true
           };
         /*FUNCTIONS BINDING*/
-          vm.editComment = editComment;
-          vm.editGrade = editGrade;
-          vm.saveDraft = saveDraft;
-          vm.printPdf = printPdf;
-          vm.sendTransaction = sendTransaction;
+         vm.startTransactions = startTransactions;
         /*SERVICES AND DATA API*/
             instanceService
               .loadAllItems()
@@ -57,79 +49,26 @@
                 vm.tableData = [].concat(tableData);
               });
         /*FUNCTIONS STRUCTURES*/
-          function editComment (event, student) {
-              event.stopPropagation(); // in case autoselect is enabled
-              
-              var editDialog = {
-                modelValue: student.comment,
-                placeholder: 'Agrega un Comentario',
-                save: function (input) {
-                  student.comment = input.$modelValue;
-                },
-                targetEvent: event,
-                title: 'Agrega un Comentario',
-                validators: {
-                  'md-maxlength': 50
-                }
-              };
-              
-              var promise;
-              promise = $mdEditDialog.small(editDialog);
-              
-              
-              promise.then(function (ctrl) {
-                var input = ctrl.getInput();
-                
-                input.$viewChangeListeners.push(function () {
-                  input.$setValidity('test', input.$modelValue !== 'test');
+         function startTransactions (ev) {
+            /*  This function Creates a confirmation to start the transactions (Actas) and then start the flow.
+             *  Strategy:
+             *  1. Create a Dialog from the components library
+             *  2. Utilize the event binding.
+             */
+            var confirm = $mdDialog.confirm()
+              .title('Activando Actas')
+              .textContent('¿Estas Seguro de iniciar ' + vm.selected.length + ' Actas?')
+              .ariaLabel('Iniciando Actas')
+              .targetEvent(ev)
+              .ok('Si')
+              .cancel('Cancelar');
+              $mdDialog.show(confirm).then(function() {
+                  console.log('Confirmando las Actas');
+                  //TODO what u have to do with the confirmaction
+                }, function() {
+                  console.log('No estuvo seguro');
                 });
-              });
-          };
-          function editGrade (event, student) {
-              event.stopPropagation(); // in case autoselect is enabled
-              
-              var editDialog = {
-                modelValue: student.comment,
-                placeholder: 'Agrega la Calificacion',
-                save: function (input) {
-                  student.grade = input.$modelValue;
-                },
-                targetEvent: event,
-                title: 'Agrega la Calificacion',
-                validators: {
-                  'md-maxlength': 4,
-                  type:"number"
-                }
-              };
-              
-              var promise;
-              promise = $mdEditDialog.small(editDialog);
-              
-              
-              promise.then(function (ctrl) {
-                var input = ctrl.getInput();
-                
-                input.$viewChangeListeners.push(function () {
-                  input.$setValidity('test', input.$modelValue !== 'test');
-                });
-              });
-          };
-          function saveDraft() {
-              //console.log($rootScope.previousState);
-              $state.go($rootScope.previousState);
           }
-          function printPdf() {
-              console.log("Previewing the PDF");
-          }
-          function sendTransaction() {
-              console.log("Starting the transaction sender");
-          }
-    
-
-    
-
-
-    
 
 
   }
