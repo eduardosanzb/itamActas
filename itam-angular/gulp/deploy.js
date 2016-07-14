4603a0b8c5ab5199ca1e172eafd81a6246f84745
eduardosanzb <eduardosanzb@gmail.com>
2016-07-13 19:05:01 -0500

    deploy

--- itam-angular/gulp/deploy.js
+++ itam-angular/gulp/deploy.js
@@ -2,8 +2,10 @@
 
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
-
+var options = {
+  origin: "github"
+}
 gulp.task('deploy', function() {
   return gulp.src('./dist/**/*')
-      .pipe(ghPages());
+      .pipe(ghPages(options));
 });
